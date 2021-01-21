3a616722f59d64bd160c1b163974f01e608fb4e9
Amedeo d'Amore <amedeodamore@gmail.com>
2021-01-21 22:14:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,14 @@ function logWhisper(string) {
 console.log(string.toLowerCase());  
 
 }
+
+function sayHitToGrandma(string) {
+if (string.toUpperCase()) {
+  return "YES INDEED!"
+} else if (string.toLowerCase()) {
+  return "I can't hear you!"
+}
+else if (string === "I love you, Grandma") {
+  return "I love you, too."
+}
+}
